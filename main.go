1cbf9d6aa38012a1157d0490d43ae4fdf55dde95
johnblat64 <johnblat64@protonmail.com>
2023-04-19 09:40:15 -0400

    make queries better

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"log"
+	"math"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	sqlBuilder "github.com/huandu/go-sqlbuilder"
@@ -46,44 +49,164 @@ type Whatever struct {
 
 const DEFAULT_COUNT = "20"
 
+func addWhereClausesFromQueryParameters(sb *sqlBuilder.SelectBuilder, c *gin.Context) {
+	// game := c.Query("stepstype")
+	// if game != "" {
+	// 	sb.Where(sb.Like("chart.stepstype", game+"%"))
+	// }
+
+	stepsType := c.Query("stepstype")
+	if stepsType != "" {
+		sb.Where(sb.E("lower(chart.stepstype)", strings.ToLower(stepsType)))
+	}
+
+	bpmMin := c.DefaultQuery("bpmMin", "0")
+	bpmMax := c.DefaultQuery("bpmMax", "999")
+
+	sb.Where(sb.Between("song_bpm.song_bpm", bpmMin, bpmMax))
+
+	packTitle := c.Query("pack")
+	if packTitle != "" {
+		sb.Where(sb.Like("lower(pack.title)", "%"+strings.ToLower(packTitle)+"%"))
+	}
+
+	timeSignatureNumerator := c.Query("timeSignatureNumerator")
+	if timeSignatureNumerator != "" {
+		sb.Where(sb.E("song_time_signature.time_signature_numerator", timeSignatureNumerator))
+	}
+
+	timeSignatureDenominator := c.Query("timeSignatureDenominator")
+	if timeSignatureDenominator != "" {
+		sb.Where(sb.E("song_time_signature.time_signature_denominator", timeSignatureDenominator))
+	}
+
+	songTitle := c.Query("title")
+	if songTitle != "" {
+		sb.Where(sb.Like("lower(song.title)", "%"+strings.ToLower(songTitle)+"%"))
+	}
+
+	songArtist := c.Query("artist")
+	if songArtist != "" {
+		sb.Where(sb.Like("lower(song.artist)", "%"+strings.ToLower(songArtist)+"%"))
+	}
+
+	chartDifficultyMeterMin := c.Query("chartDifficultyMeterMin")
+	chartDifficultyMeterMax := c.Query("chartDifficultyMeterMax")
+	if chartDifficultyMeterMin != "" && chartDifficultyMeterMax != "" {
+		sb.Where(sb.Between("chart.meter", chartDifficultyMeterMin, chartDifficultyMeterMax))
+	}
+
+	chartCredit := c.Query("chartCredit")
+	if chartCredit != "" {
+		sb.Where(sb.Like("lower(chart.credit)", "%"+strings.ToLower(chartCredit)+"%"))
+	}
+}
+
 func getFilteredSongs(c *gin.Context) {
-	sqlSelectBuilder := sqlBuilder.NewSelectBuilder()
+	sbFilteredSongs := sqlBuilder.NewSelectBuilder()
 
-	sqlSelectBuilder.Select("song_chart_json")
-	sqlSelectBuilder.From("filtered_songs_with_charts('',0,999,4,4,'','pump-single',	0, 99, '')")
-
-	sqlQuery, _ := sqlSelectBuilder.BuildWithFlavor(sqlBuilder.PostgreSQL)
-
-	rows, err := gDb.Query(sqlQuery)
+	// get pageStr from query param
+	pageStr := c.DefaultQuery("page", "1")
+	page, err := strconv.Atoi(pageStr)
 	if err != nil {
 		gSugar.Error(err)
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
-	if rows.Err() != nil {
+	DEFAULT_COUNT := "20"
+	MAX_COUNT := 100
+	// get pageSizeStr from query param
+	pageSizeStr := c.DefaultQuery("pageSize", DEFAULT_COUNT)
+	pageSize, err := strconv.Atoi(pageSizeStr)
+	if err != nil {
+		gSugar.Error(err)
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	if pageSize > MAX_COUNT {
+		pageSize = MAX_COUNT
+	}
+
+	// Count of songs in subquery
+	sbCountFilteredSongs := sqlBuilder.NewSelectBuilder()
+	sbCountFilteredSongs.Select("COUNT( DISTINCT song.songid)").As("count", "count")
+	sbCountFilteredSongs.From("song")
+	sbCountFilteredSongs.Join("pack_song_map", "pack_song_map.songid = song.songid")
+	sbCountFilteredSongs.Join("pack", "pack.packid = pack_song_map.packid")
+	sbCountFilteredSongs.Join("chart", "chart.songid = song.songid")
+	sbCountFilteredSongs.Join("song_bpm", "song_bpm.songid = song.songid")
+	sbCountFilteredSongs.Join("song_time_signature", "song_time_signature.songid = song.songid")
+	addWhereClausesFromQueryParameters(sbCountFilteredSongs, c)
+	countQuery, args := sbCountFilteredSongs.BuildWithFlavor(sqlBuilder.PostgreSQL)
+	// get filteredSongsCount value in filteredSongsCount struct
+	var filteredSongsCount Count
+	countRow := gDb.QueryRow(countQuery, args...)
+	err = countRow.Scan(&filteredSongsCount.Count)
+	if err != nil {
+		gSugar.Error(err)
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	offset := (page - 1) * pageSize
+	limit := pageSize
+	pageCount := int(math.Ceil(float64(filteredSongsCount.Count) / float64(pageSize)))
+	totalSongsCount := filteredSongsCount.Count
+
+	// Subquery
+	sbFilteredSongs.Select("song.songid, song.title").Distinct().As("song.songid", "songid")
+	sbFilteredSongs.From("song")
+	sbFilteredSongs.Join("pack_song_map", "pack_song_map.songid = song.songid")
+	sbFilteredSongs.Join("pack", "pack.packid = pack_song_map.packid")
+	sbFilteredSongs.Join("chart", "chart.songid = song.songid")
+	sbFilteredSongs.Join("song_bpm", "song_bpm.songid = song.songid")
+	sbFilteredSongs.Join("song_time_signature", "song_time_signature.songid = song.songid")
+	sbFilteredSongs.OrderBy("song.title ASC")
+	sbFilteredSongs.Offset(offset)
+	sbFilteredSongs.Limit(limit)
+	addWhereClausesFromQueryParameters(sbFilteredSongs, c)
+
+	// Main Query
+	sbSongData := sqlBuilder.NewSelectBuilder()
+	sbSongData.Select("song.songid, song.title, song.artist, song_bpm.song_bpm, song_time_signature.time_signature_numerator, song_time_signature.time_signature_denominator, chart.chartid, chart.chartname, chart.stepstype, chart.description, chart.chartstyle, chart.difficulty, chart.meter, chart.credit, chart.stops_count, chart.delays_count, chart.warps_count, chart.scrolls_count, chart.fakes_count, chart.speeds_count, pack.packid, pack.name")
+	sbSongData.From(sbSongData.BuilderAs(sbFilteredSongs, "filtered_songs"))
+	sbSongData.Join("song", "filtered_songs.songid = song.songid")
+	sbSongData.Join("song_bpm", "song_bpm.songid = song.songid")
+	sbSongData.Join("song_time_signature", "song_time_signature.songid = song.songid")
+	sbSongData.Join("chart", "chart.songid = song.songid")
+	sbSongData.Join("pack_song_map", "pack_song_map.songid = song.songid")
+	sbSongData.Join("pack", "pack.packid = pack_song_map.packid")
+	sbSongData.OrderBy("song.title ASC")
+
+	songDataQuery, args := sbSongData.BuildWithFlavor(sqlBuilder.PostgreSQL)
+	gSugar.Info(songDataQuery)
+	gSugar.Info("Args: ", args)
+
+	rows, err := gDb.Query(songDataQuery, args...)
+	if err != nil {
 		gSugar.Error(err)
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 	defer rows.Close()
 
-	var whatever Whatever
-	var whateverStruct = sqlBuilder.NewStruct(new(Whatever))
-
-	rows.Next()
-	if rows.Err() != nil {
-		gSugar.Error(rows.Err())
-		c.AbortWithError(http.StatusInternalServerError, err)
-		return
-	}
-
-	err = rows.Scan(whateverStruct.Addr(&whatever)...)
+	sbCount := sqlBuilder.NewSelectBuilder()
+	sbCount.Select("COUNT(songid)")
+	sbCount.From("song")
+	songResultsResponse := SongResultsResponse{}
+	songResultsResponse.PageCount = pageCount
+	songResultsResponse.Page = page
+	songResultsResponse.PageSize = pageSize
+	songResultsResponse.TotalSongsCount = totalSongsCount
+	err = carta.Map(rows, &songResultsResponse.Songs)
+	// songPage.ResultsCount = len(songPage.Songs)
 	if err != nil {
 		gSugar.Error(err)
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
-	c.Data(http.StatusOK, "application/json", whatever.X)
+
+	c.JSON(200, songResultsResponse)
 }
 
 func getSongById(c *gin.Context) {
@@ -97,13 +220,10 @@ func getSongById(c *gin.Context) {
 	sqlSongSelectBuilder.Join("chart", "chart.songid = song.songid")
 	sqlSongSelectBuilder.Join("song_bpm", "song_bpm.songid = song.songid")
 	sqlSongSelectBuilder.Join("song_time_signature", "song_time_signature.songid = song.songid")
-	sqlSongSelectBuilder.Where("song.songid='" + songid + "'")
-	sqlSongQuery, _ := sqlSongSelectBuilder.BuildWithFlavor(sqlBuilder.PostgreSQL)
+	sqlSongSelectBuilder.Where(sqlSongSelectBuilder.E("song.songid", songid))
+	sqlSongQuery, args := sqlSongSelectBuilder.BuildWithFlavor(sqlBuilder.PostgreSQL)
 
-	// var songRow SongRow
-	// var charts []Chart
-
-	rows, err := gDb.Query(sqlSongQuery)
+	rows, err := gDb.Query(sqlSongQuery, args...)
 	defer rows.Close()
 	if err != nil {
 		gSugar.Errorln(err)
@@ -117,7 +237,7 @@ func getSongById(c *gin.Context) {
 		gSugar.Errorln(err)
 	}
 
-	c.JSON(200, songs)
+	c.JSON(200, songs[0])
 }
 
 /**
--- song.go
+++ song.go
@@ -1,40 +1,63 @@
 package main
 
 type Chart struct {
-	Chartid     string `json:"chartId" `
-	Chartname   string `json:"name" `
-	Stepstype   string `json:"stepsType" `
-	Description string `json:"description" `
-	Chartstyle  string `json:"chartStyle" `
-	Difficulty  string `json:"difficulty" `
-	Meter       int    `json:"meter" `
-	Credit      string `json:"credit" `
-	// StopsCount   int    `json:"stopsCount" db:"chart.stops_count"`
-	// DelaysCount  int    `json:"delaysCount" db:"chart.delays_count"`
-	// WarpsCount   int    `json:"warpsCount" db:"chart.warps_count"`
-	// ScrollsCount int    `json:"scrollsCount" db:"chart.scrolls_count"`
-	// FakesCount   int    `json:"fakesCount" db:"chart.fakes_count"`
-	// SpeedsCount  int    `json:"speedsCount" db:"chart.speeds_count"`
+	Chartid      string `json:"chartId" `
+	Chartname    string `json:"name" `
+	Stepstype    string `json:"stepsType" `
+	Description  string `json:"description" `
+	Chartstyle   string `json:"chartStyle" `
+	Difficulty   string `json:"difficulty" `
+	Meter        int    `json:"meter" `
+	Credit       string `json:"credit" `
+	StopsCount   int    `json:"stopsCount" db:"stops_count"`
+	DelaysCount  int    `json:"delaysCount" db:"delays_count"`
+	WarpsCount   int    `json:"warpsCount" db:"warps_count"`
+	ScrollsCount int    `json:"scrollsCount" db:"scrolls_count"`
+	FakesCount   int    `json:"fakesCount" db:"fakes_count"`
+	SpeedsCount  int    `json:"speedsCount" db:"speeds_count"`
 }
 
 type Bpm struct {
-	Value float32 `db:"song_bpm"`
+	Value float32 `json:"value" db:"song_bpm"`
 }
 
 type TimeSignature struct {
-	Numerator   int `db:"time_signature_numerator"`
-	Denominator int `db:"time_signature_denominator"`
+	Numerator   int `json:"numerator" db:"time_signature_numerator"`
+	Denominator int `json:"denominator" db:"time_signature_denominator"`
 }
 
 type Song struct {
-	Songid         string          `json:"songId"         `
-	Title          string          `json:"songTitle"      `
-	Artist         string          `json:"songArtist"     `
+	SongId         string          `json:"songId"         `
+	Title          string          `json:"title"      `
+	Artist         string          `json:"artist"     `
 	Bpms           []Bpm           `json:"bpms"           `
-	Timesignatures []TimeSignature `json:"timeSignatures" `
+	TimeSignatures []TimeSignature `json:"timeSignatures" `
 	Charts         []Chart         `json:"charts"`
+	PackId         string          `json:"packId"         db:"packid"`
+	PackName       string          `json:"packName"       db:"name"`
+}
 
-	// 	PackId         string          `json:"packId"         db:"pack.packid"`
-	//	PackName       string          `json:"packName"       db:"pack.name"`
+type SongResultsResponse struct {
+	Page            int    `json:"pageNum"`
+	PageSize        int    `json:"pageSize"`
+	PageCount       int    `json:"pageCount"`
+	TotalSongsCount int    `json:"totalSongsCount" db:"total_songs_count"`
+	Songs           []Song `json:"songs"`
+}
 
+type SongSearchParameters struct {
+	Title                    string
+	Artist                   string
+	Credit                   string
+	Pack                     string
+	TimeSignatureNumerator   int
+	TimeSignatureDenominator int
+	BpmMin                   float64
+	BpmMax                   float64
+	StepsType                string
+}
+
+// struct for counting songs
+type Count struct {
+	Count int `db:"count"`
 }
